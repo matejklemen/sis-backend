04e32c714a447a85d759e392948ec040c71b842c
Matej Klemen <mk3141@student.uni-lj.si>
2018-03-20 18:49:36 +0100

    Add servlet for testing

---
+++ api/src/main/java/TryFunctionality.java
@@ -0,0 +1,29 @@
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Logger;
+@WebServlet("/servlet")
+public class TryFunctionality extends HttpServlet {
+    private static final Logger log = Logger.getLogger("JPAServlet");
+    @Inject
+    private UserLoginBean ulB;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        List<UserLogin> userlogins = ulB.getAllUserLoginInfo();
+
+        for(UserLogin u : userlogins) {
+            log.info(String.format("IDuser: %d\nUsername: %s\nPassword: %s\nRole: %s\nSalt: %d\n",
+                    u.getIdUser(), u.getUsername(), u.getPassword(), u.getRole(), u.getSalt()));
+            writer.append(String.format("IDuser: %d\nUsername: %s\nPassword: %s\nRole: %s\nSalt: %d\n",
+                    u.getIdUser(), u.getUsername(), u.getPassword(), u.getRole(), u.getSalt()));
+        }
+        writer.close();
+    }
+}
