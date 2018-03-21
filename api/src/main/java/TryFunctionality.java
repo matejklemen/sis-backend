627f2a2d6df2e2f8ce271674202b1f3c169ed770
Matej Klemen <mk3141@student.uni-lj.si>
2018-03-21 23:49:31 +0100

    Add a few examples into servlet

--- api/src/main/java/TryFunctionality.java
+++ api/src/main/java/TryFunctionality.java
@@ -16,12 +16,22 @@ public class TryFunctionality extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
+
+        ulB.insertUserLoginSingle("newUser",
+                "5c531afb2e4106cc78f99ff895ef99d2fa587f7272a91f82244f06caddf89176c299afc0ef4a447d6c5042dff4690e271a3d09aabf97465faba20591f818ab27",
+                "admin");
+
         List<UserLogin> userlogins = ulB.getAllUserLoginInfo();
+
+        boolean couldUserLogin = ulB.authenticateUser("newUser", "1234");
+
+        writer.append("Could user newUser potentially log in?" + couldUserLogin);
+
 
         for(UserLogin u : userlogins) {
             log.info(String.format("IDuser: %d\nUsername: %s\nPassword: %s\nRole: %s\nSalt: %d\n",
                     u.getIdUser(), u.getUsername(), u.getPassword(), u.getRole(), u.getSalt()));
-            writer.append(String.format("IDuser: %d\nUsername: %s\nPassword: %s\nRole: %s\nSalt: %d\n",
+            writer.append(String.format("IDuser: %d\nUsername: %s\nPassword: %s\nRole: %s\nSalt: %d\n----\n",
                     u.getIdUser(), u.getUsername(), u.getPassword(), u.getRole(), u.getSalt()));
         }
         writer.close();
