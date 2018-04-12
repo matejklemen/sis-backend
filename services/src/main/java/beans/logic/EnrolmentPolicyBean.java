aa83dded8008a310ed5ab00bd7d3d8c532acbc09
Jernej <jernej.cernelcgmail.com>
2018-04-12 17:06:21 +0200

    tmp

--- services/src/main/java/beans/logic/EnrolmentPolicyBean.java
+++ services/src/main/java/beans/logic/EnrolmentPolicyBean.java
@@ -1,11 +1,8 @@
 package beans.logic;
 
-import beans.crud.CurriculumBean;
-import beans.crud.EnrolmentBean;
-import beans.crud.GradeBean;
-import entities.Enrolment;
-import entities.Grade;
-import entities.Student;
+import beans.crud.*;
+import entities.*;
+import entities.logic.EnrolmentSheet;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -24,6 +21,30 @@ public class EnrolmentPolicyBean {
 
     @Inject
     private EnrolmentBean eb;
+
+    @Inject
+    private PostAddressBean postAddressBean;
+
+    @Inject
+    private CountryBean countryBean;
+
+    @Inject
+    private MunicipalityBean municipalityBean;
+
+    @Inject
+    private StudyProgramBean studyProgramBean;
+
+    @Inject
+    private StudyTypeBean studyTypeBean;
+
+    @Inject
+    private StudyDegreeBean studyDegreeBean;
+
+    @Inject
+    private StudyFormBean studyFormBean;
+
+    @Inject
+    private StudyKindBean studyKindBean;
 
     public boolean hasStudentFreeChoiceOfCurriculum(Student s){
         if(eb.getEnrolmentsForStudent(s.getId()).isEmpty())
@@ -49,5 +70,32 @@ public class EnrolmentPolicyBean {
             return true;
 
         return false;
+    }
+
+    public List<String> validCodes(EnrolmentSheet es){
+        List<String> list = new ArrayList<String>();
+        if(!postAddressBean.existsPostAddress(es.getStudent().getAddress1().getPost().getId())) {
+            list.add("Address1 invalid post code");
+        }
+        if(es.getStudent().getAddress2() != null && !postAddressBean.existsPostAddress(es.getStudent().getAddress2().getPost().getId())) {
+            list.add("Address2 invalid post code");
+        }
+        if(!municipalityBean.existsMunicipality(es.getStudent().getMunicipalityOfBirth().getId())) {
+            list.add("Invalid municipality code");
+        }
+        if(!countryBean.existsCountry(es.getStudent().getCountryOfBirth().getId())) {
+            list.add("Invalid country code");
+        }
+        if(!studyProgramBean.existsStudyProgram(es.getEnrolmentToken().getStudyProgram().getId())) {
+            list.add("Invalid study program code");
+        }
+        if(!studyTypeBean.existsStudyType(es.getEnrolmentToken().getType().getId())) {
+            list.add("Invalid study type code");
+        }
+        if(!studyTypeBean.existsStudyType(es.getEnrolmentToken().getType().getId())) {
+            list.add("Invalid study type code");
+        }
+        year >= 1 && year <= 3
+        return true;
     }
 }
