31f8cb2507ae5069ecb5a9c36cc9c4edc87ad183
Blazka Blatnik <bb3172@student.uni-lj.si>
2018-04-17 13:20:30 +0200

    #3: fix pairs names

--- services/src/main/java/utils/Pair.java
+++ services/src/main/java/utils/Pair.java
@@ -2,24 +2,24 @@ package utils;
 
 public class Pair<K, V> {
 
-    private final K element0;
-    private final V element1;
+    private final K first;
+    private final V second;
 
     public static <K, V> Pair<K, V> createPair(K element0, V element1) {
         return new Pair<K, V>(element0, element1);
     }
 
     public Pair(K element0, V element1) {
-        this.element0 = element0;
-        this.element1 = element1;
+        this.first = element0;
+        this.second = element1;
     }
 
-    public K getElement0() {
-        return element0;
+    public K getFirst() {
+        return first;
     }
 
-    public V getElement1() {
-        return element1;
+    public V getSecond() {
+        return second;
     }
 
 }
