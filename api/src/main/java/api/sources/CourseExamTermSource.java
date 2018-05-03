de18a1819038b1eb93ea8eca4cdc9a7431b853d3
Matej Klemen <mk3141@student.uni-lj.si>
2018-05-03 16:20:08 +0200

    Added documentation for course exam term source

--- api/src/main/java/api/sources/CourseExamTermSource.java
+++ api/src/main/java/api/sources/CourseExamTermSource.java
@@ -5,6 +5,12 @@ import beans.crud.CourseExamTermBean;
 import beans.logic.CourseExamTermValidationBean;
 import com.kumuluz.ee.rest.beans.QueryParameters;
 import entities.curriculum.CourseExamTerm;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import pojo.ResponseError;
@@ -37,6 +43,19 @@ public class CourseExamTermSource {
     @Inject
     private CourseExamTermBean cetb;
 
+    @Operation(description = "Returns a list of all exam terms.", summary = "Get list of exam terms",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "List of exam terms",
+                            content = @Content(
+                                    schema = @Schema(implementation = CourseExamTerm.class))
+                    )},
+            parameters = {
+                    @Parameter(name = "offset", description = "Starting point",in = ParameterIn.QUERY),
+                    @Parameter(name = "limit", description = "Number of returned entities", in = ParameterIn.QUERY),
+                    @Parameter(name = "order", description = "Order", in = ParameterIn.QUERY),
+                    @Parameter(name = "filter", description = "Filter the returned results, more info at https://github.com/kumuluz/kumuluzee-rest#examples", in = ParameterIn.QUERY)
+            })
     @GET
     public Response getAllExamTerms() {
         QueryParameters query = QueryParameters.query(uriInfo.getRequestUri().getQuery()).build();
@@ -54,17 +73,42 @@ public class CourseExamTermSource {
                 .build();
     }
 
+    @Operation(description = "Returns an exam term object for specified ID of exam term.", summary = "Get exam term by ID",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Exam term object for specified ID",
+                            content = @Content(
+                                    schema = @Schema(implementation = CourseExamTerm.class))
+                    ),
+                    @ApiResponse(responseCode = "404",
+                            description = "Exam term with this ID not found",
+                            content = @Content(
+                                    schema = @Schema(implementation = ResponseError.class)
+                            ))
+            })
     @GET
     @Path("{id}")
-    public Response getExamTermByIdExamTerm(@PathParam("id") int id) {
+    public Response getExamTermByIdExamTerm(@PathParam("id") @Parameter(name = "id", description = "ID of wanted exam term object", in = ParameterIn.PATH)
+                                                        int id) {
         CourseExamTerm cet = cetb.getExamTermById(id);
 
         return cet == null? Response.status(Response.Status.NOT_FOUND).build():
                 Response.status(Response.Status.OK).entity(cet).build();
     }
 
+    @Operation(description = "Insert a new exam term object.", summary = "Insert exam term",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Exam term successfully inserted"
+                    ),
+                    @ApiResponse(responseCode = "400",
+                            description = "Inserting new exam term failed",
+                            content = @Content(
+                                    schema = @Schema(implementation = ResponseError.class)
+                            ))
+            })
     @PUT
-    public Response insertExamTerm(CourseExamTerm cet) {
+    public Response insertExamTerm(@Parameter(description = "New course exam term object") CourseExamTerm cet) {
         List<String> errors = cetvb.validateExamTermData(cet);
 
         if(errors.isEmpty()) {
@@ -77,8 +121,21 @@ public class CourseExamTermSource {
                     new ResponseError(400, errors.toArray(new String[0]))).build();
     }
 
+    @Operation(description = "Update exam term object with same ID as the one provided.", summary = "Update exam term",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Updated exam term object",
+                            content = @Content(
+                                    schema = @Schema(implementation = CourseExamTerm.class))
+                    ),
+                    @ApiResponse(responseCode = "400",
+                            description = "Updating exam term failed",
+                            content = @Content(
+                                    schema = @Schema(implementation = ResponseError.class)
+                            ))
+            })
     @POST
-    public Response updateExamTerm(CourseExamTerm updatedCet) {
+    public Response updateExamTerm(@Parameter(description = "Course exam term with updated fields") CourseExamTerm updatedCet) {
         List <String> errors = cetvb.validateExamTermData(updatedCet);
 
         if(errors.isEmpty()) {
@@ -91,9 +148,22 @@ public class CourseExamTermSource {
                     new ResponseError(400, errors.toArray(new String[0]))).build();
     }
 
+    @Operation(description = "Delete exam term object with specified ID.", summary = "Delete exam term",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Deleted exam term object"
+                    ),
+                    @ApiResponse(responseCode = "400",
+                            description = "Deleting exam term failed",
+                            content = @Content(
+                                    schema = @Schema(implementation = ResponseError.class)
+                            )
+                    )
+            })
     @DELETE
     @Path("{id}")
-    public Response deleteExamTerm(@PathParam("id") int idExamTerm) {
+    public Response deleteExamTerm(@PathParam("id") @Parameter(description = "ID of exam term to delete", in = ParameterIn.PATH)
+                                               int idExamTerm) {
         try {
             cetb.deleteExamTerm(idExamTerm);
             return Response.status(Response.Status.OK).build();
